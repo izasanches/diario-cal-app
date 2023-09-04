f0fab5b2a9e421df587c0d4f4c05598b0e2252ad
Izabele Sanches de Medeiros <izabele.sanches@hotmail.com>
2023-09-03 21:15:01 -0300

    feat: add menu

--- app/src/main/java/com/projeto/diariocal/MenuActivity.java
+++ app/src/main/java/com/projeto/diariocal/MenuActivity.java
@@ -3,10 +3,14 @@ package com.projeto.diariocal;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
@@ -15,6 +19,26 @@ public class MenuActivity extends AppCompatActivity {
     private ListView listViewAlimento;
     private ArrayList<Alimento> alimentos;
     private AlimentoAdapter listaAdapter;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.menuItemAdicionar) {
+            adicionarAlimento();
+            return true;
+        }
+
+        else if (itemId == R.id.menuItemSobre) {
+            abrirSobre();
+            return true;
+        }
+
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +76,16 @@ public class MenuActivity extends AppCompatActivity {
         AlimentoActivity.novoAlimento(this);
     }
 
+    public void adicionarAlimento() {
+        AlimentoActivity.novoAlimento(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.principal_opcoes, menu);
+        return true;
+    }
+
     @Override
     protected void onActivityResult(int requestCode,
                                     int resultCode,
@@ -81,6 +115,10 @@ public class MenuActivity extends AppCompatActivity {
     }
 
     public void abrirSobre(View view){
+        SobreActivity.sobre(this);
+    }
+
+    public void abrirSobre(){
         SobreActivity.sobre(this);
     }
 }
